a0b41c90aacd3b7a3b49527fb03b26810882edf2
lili-sid <lili0allen@gmail.com>
2015-05-15 09:24:05 +1000

    not finished yet

---
+++ app/code/local/EasyMode/Customfields/Block/Adminhtml/Customfields/Grid.php
@@ -0,0 +1,65 @@
+<?php
+class EasyMode_Customfields_Block_Adminhtml_Customfields_Grid extends Mage_Adminhtml_Block_Widget_Grid
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->setId('customfieldsGrid');
+		$this->setDefaultSort('customfields_id');
+		$this->setDefaultDir('ASC');
+		$this->setSaveParametersInSession(true);
+		$this->setUseAjax(true);
+
+	}
+
+	protected function _prepareCollection()
+	{
+		$collection = Mage::getModel('customfields/customfields')->getCollection();
+		$this->setCollection($collection);
+		return parent::_prepareCollection();
+	}
+
+	protected function _prepareColumns()
+	{
+		$this->addColumn('customfields_id', array(
+			'header'	=> Mage::helper('customfields')->__('ID'),
+			'align'		=> 'right',
+			'width'		=> '50px',
+			'index'		=> 'customfields_id',
+			));
+
+		$this->addColumn('title', array(
+			'header'	=> Mage::helper('customfields')->__('Title'),
+			'align'		=> 'left',
+			'index'		=> 'title',
+			));
+
+		$this->addColumn('group', array(
+			'header'	=> Mage::helper('customfields')->__('Group'),
+			'align'		=> 'left',
+			'index'		=> 'group',
+			));
+
+		$this->addColumn('content', array(
+			'header'	=> Mage::helper('customfields')->__('Content'),
+			'align'		=> 'left',
+			'index'		=> 'content',
+			));
+
+
+
+
+		return parent::_prepareColumns();
+	}
+
+	public function getRowUrl($row)
+	{
+		return $this->getUrl('*/*/edit', array('id' => $row->getId()));
+	}
+
+	public function getGridUrl()
+	{
+		return $this->getUrl('*/*/grid', array('_current' => true));
+	}
+
+}
